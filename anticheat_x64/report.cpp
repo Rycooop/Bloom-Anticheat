8b411041c7b92f7c46baaa47275a8e7e6ccc06e8
Rycooop <45408021+Rycooop@users.noreply.github.com>
2022-03-03 10:02:11 -0700

    Add files via upload

---
+++ anticheat_x64/report.cpp
@@ -0,0 +1,44 @@
+#include "includes.h"
+
+
+void Report::SendReport(DWORD reportCode)
+{
+	AllocConsole();
+	FILE* f;
+	freopen_s(&f, "conout$", "w", stdout);
+	system("Color 0C");
+
+	std::cout << "Anticheat has encountered the following issue:\n " << std::endl;
+
+	switch (reportCode)
+	{
+		case INVALID_ANTICHEAT_START:
+		{
+			std::cout << "[*] Anticheat DLL failed to initialize correctly, closing..." << std::endl;
+			break;
+		}
+		case MANUALMAPPED_MODULE:
+		{
+			std::cout << "[*] Manually maped module detected, closing..." << std::endl;
+			break;
+		}
+		case HOOK_DETECTED:
+		{
+			std::cout << "[*] Hook has been detected, closing..." << std::endl;
+			break;
+		}
+		case DEBUGGER_DETECTED:
+		{
+			std::cout << "[*] Debugger was detected, closing..." << std::endl;
+			break;
+		}
+		case BLACKLISTED_DLL_DETECTED:
+		{
+			std::cout << "[*] Blacklisted DLL attempted to load, closing..." << std::endl;
+			break;
+		}
+	}
+
+	Sleep(3000);
+	ExitProcess(0);
+}
---
+++ anticheat_x64/report.h
@@ -0,0 +1,16 @@
+#include <Windows.h>
+
+
+namespace Report
+{
+	void SendReport(DWORD reportCode);
+}
+
+enum reports
+{
+	INVALID_ANTICHEAT_START = 1,
+	MANUALMAPPED_MODULE,
+	HOOK_DETECTED,
+	DEBUGGER_DETECTED,
+	BLACKLISTED_DLL_DETECTED
+};
